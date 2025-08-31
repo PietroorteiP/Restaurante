271c2dc5c0db691a5637824a7675cf4313b26a7e
Pietro <pietros.ifpr2024@gmail.com>
2025-08-30 21:03:03 -0300

    Create Pedido.php

---
+++ modelo/Pedido.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once("Prato.php");
+
+class Pedido{
+    //Atributos
+    private string $nomeCliente;
+    private string $nomeGarcom;
+    private Prato $prato;
+
+    //Métodos
+    public function __toString(){
+        $info = " O cliente " . $this->nomeCliente . ", foi atendido pelo garçom " . $this->nomeGarcom . ", pediu um prato de "
+        . $this->prato->getNome() . " no valor de R$" . $this->prato->getValor();
+        return $info;
+    }
+    
+
+    /**
+     * Get the value of nomeCliente
+     */
+    public function getNomeCliente(): string
+    {
+        return $this->nomeCliente;
+    }
+
+    /**
+     * Set the value of nomeCliente
+     */
+    public function setNomeCliente(string $nomeCliente): self
+    {
+        $this->nomeCliente = $nomeCliente;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nomeGarcom
+     */
+    public function getNomeGarcom(): string
+    {
+        return $this->nomeGarcom;
+    }
+
+    /**
+     * Set the value of nomeGarcom
+     */
+    public function setNomeGarcom(string $nomeGarcom): self
+    {
+        $this->nomeGarcom = $nomeGarcom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prato
+     */
+    public function getPrato(): Prato
+    {
+        return $this->prato;
+    }
+
+    /**
+     * Set the value of prato
+     */
+    public function setPrato(Prato $prato): self
+    {
+        $this->prato = $prato;
+
+        return $this;
+    }
+}
